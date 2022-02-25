d3f4c9fe2b10155c067096a493a120f05d44385a
puja12 <prabhupuja12@gmail.com>
2022-02-25 11:46:41 +0530

    diet section

---
+++ app/Http/Controllers/DietsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Diet;
+use Illuminate\Http\Request;
+
+class DietsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Diet  $diet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Diet $diet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Diet  $diet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Diet $diet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Diet  $diet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Diet $diet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Diet  $diet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Diet $diet)
+    {
+        //
+    }
+}
